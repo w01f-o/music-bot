06df0013151c460fb6de93a469bbfaf794476df6
w01f <arsenimarchenko@bk.ru>
2024-03-20 01:09:13 +0300

    работает api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,23 @@ import express, { NextFunction } from 'express';
 import { Request, Response } from 'express';
 import getLocalTracks from '../utility/getLocalTracks.js';
 const app = express();
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+class Track {
+  name: string;
+  author: string;
+  id: number;
+  url: string;
+
+  constructor(name: string, author: string, url: string) {
+    this.name = name;
+    this.author = author;
+    this.id = Math.round(Math.random() * 10000);
+    this.url = url;
+  }
+}
 
 export const startApi = () => {
   app.use(express.json());
@@ -11,12 +28,26 @@ export const startApi = () => {
     next();
   });
 
-  //   app.use('/tracks', (req, res) => {
-  //     res.json({ test: getLocalTracks() });
-  //   });
+  const __dirname: string = path.dirname(fileURLToPath(import.meta.url));
+  const tracksPath: string = path.join(__dirname, '..', '..', 'audio');
+  const tracksFiles: string[] = fs.readdirSync(tracksPath);
 
-  app.use('/tracks/sad.mp3', (req, res) => {
-    res.sendFile(getLocalTracks()[1].value);
+  const tracks: Track[] = tracksFiles.map(
+    (track: string, index: number) =>
+      new Track(
+        track.split(' - ')[0],
+        track.split(' - ')[1].replace(/\.[^/.]+$/, ''),
+        `http://localhost:8000/api/tracks/${index}.mp3`
+      )
+  );
+
+  app.use('/api/tracks/:trackIndex.mp3', (req, res) => {
+    const trackIndex = req.params.trackIndex;
+    res.sendFile(getLocalTracks()[+trackIndex].value);
+  });
+
+  app.use('/api/tracks', (req, res) => {
+    res.json({ tracks: tracks });
   });
 
   const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
